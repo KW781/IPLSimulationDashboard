274adbe868b83a1ca7a428ce680d1bd44714cdb4
Aarnob Guha <aarnobguha@gmail.com>
2022-04-01 04:30:01 +1300

    Added initial header in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,10 +3,21 @@ import classes from './PageStylings.module.css';
 function HomePage() {
   return (
     <div>
-      <div />
-      <header className={classes.homeHeading}>
+      <header className={classes.generalHeading}>
       Welcome to the IPL Simulation!
       </header>
+      <body>
+        <ul className={classes.ul}>
+          <li>
+          This is a text-based game that simulates the Indian Premier League!
+          </li>
+          <li>
+          Play as an IPL team, buy your favourite players, and go head to head
+          against computer automated franchises in a series of matches to try
+          and win the title!
+          </li>
+        </ul>
+      </body>
     </div>
   );
 }
