7038c667edc990387c346894a54e036dd58cf598
Aarnob Guha <aarnobguha@gmail.com>
2022-05-01 04:25:28 +1200

    initial progress on deedback sharing page

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -1,6 +1,24 @@
+import classes from './PageStylings.module.css';
+
 function FeedbackPage() {
   return (
-    <div>Feedback Page - Still in development</div>
+    <div>
+      <div className={classes.generalHeading}>Share your feedback!</div>
+      <ul className={classes.ul}>
+        <li>
+        Share what you think about the game, and any improvements that could be
+        made!
+        </li>
+        <li>
+        Your feedback will be read and taken into account when adding new
+        features to future versions.
+        </li>
+      </ul>
+      <form className={classes.form}>
+        <div>Rate IPL Simulation</div>
+        <input type='text' required id='rating' />
+      </form>
+    </div>
   );
 }
 
