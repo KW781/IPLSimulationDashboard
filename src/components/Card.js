5d7e9f16042e2baa7e7c109c34392aac5775ecb1
Aarnob Guha <aarnobguha@gmail.com>
2022-07-03 00:43:10 +1200

    fixed positioning of card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import classes from './CardStyling.module.css';
 
-function Card(properties) {
+function Card() {
   return (
-    <div className={classes.card}>
-      {properties.children}
+    <div>
+      <div className={classes.separator} /> /* create black separator between cards*/
+      <div className={classes.card}>
+        <header>
+          Testing stupid CSS stuff
+        </header>
+      </div>
     </div>
   );
 }
