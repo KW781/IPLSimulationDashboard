7f925fd4b3fe30a01aff288ea6dd55e59e767598
Aarnob Guha <aarnobguha@gmail.com>
2022-07-05 03:03:01 +1200

    completed star rating component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,6 +4,7 @@ import classes from "./StarRatingStyling.module.css";
 function StarRating() {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+
   return (
     <div className="star-rating">
       {[...Array(5)].map((star, index) => {
@@ -12,7 +13,7 @@ function StarRating() {
           <button
             type="button"
             key={index}
-            className={index <= (hover || rating) ? "on" : "off"}
+            className={index > (hover || rating) ? classes.on : classes.off}
             onClick={() => setRating(index)}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
