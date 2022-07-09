18297e33b81e03189b96fa252bdcdaaab05a7b6b
Aarnob Guha <aarnobguha@gmail.com>
2022-07-09 23:52:00 +1200

    enabled submission of ratings to firebase

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,10 +1,25 @@
 import { useState } from 'react';
+import { firestore } from '../firebase/firebaseSetup.js';
+import { updateDoc, getDoc, doc } from '@firebase/firestore';
 import classes from './StarRatingStyling.module.css';
 import SubmitButton from './SubmitButton.js';
 
 function StarRating() {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+
+  async function updateRating() {
+    const ratingsRef = doc(firestore, 'ratings', 'avg-rating');
+    const docSnap = await getDoc(ratingsRef);
+    const currNumRatings = docSnap.data().numRatings;
+    const currAvgRating = docSnap.data().rating;
+    const newAvgRating = (currAvgRating * currNumRatings + rating) / (currNumRatings + 1);
+
+    await updateDoc(ratingsRef, {
+      rating: newAvgRating,
+      numRatings: currNumRatings + 1
+    });
+  }
 
   return (
     <div>
@@ -25,7 +40,7 @@ function StarRating() {
           );
         })}
       </div>
-      <SubmitButton />
+      <SubmitButton uponClick={updateRating}/>
     </div>
   );
 }
