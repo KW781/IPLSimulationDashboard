34698b36f049664e7ad312f50c56e82b8048a879
Aarnob Guha <aarnobguha@gmail.com>
2022-07-11 13:59:17 +1200

    disabled stars upon rating submission

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -7,18 +7,23 @@ import SubmitButton from './SubmitButton.js';
 function StarRating() {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [disableStars, setDisableStars] = useState(false);
 
   async function updateRating() {
-    const ratingsRef = doc(firestore, 'ratings', 'avg-rating');
-    const docSnap = await getDoc(ratingsRef);
+    const ratingsRef = doc(firestore, 'ratings', 'avg-rating'); /* retrieve the document ratings document reference */
+    const docSnap = await getDoc(ratingsRef); /* get the document itself from the reference */
     const currNumRatings = docSnap.data().numRatings;
     const currAvgRating = docSnap.data().rating;
+    /* calculate the new average rating */
     const newAvgRating = (currAvgRating * currNumRatings + rating) / (currNumRatings + 1);
 
+    /* update the document with the new rating */
     await updateDoc(ratingsRef, {
       rating: newAvgRating,
       numRatings: currNumRatings + 1
     });
+
+    setDisableStars(true); /* disable the stars once submit button is clicked */
   }
 
   return (
@@ -31,10 +36,21 @@ function StarRating() {
               type="button"
               key={index}
               className={index > (hover || rating) ? classes.on : classes.off}
-              onClick={() => setRating(index)}
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(rating)}
-            >
+              onClick={() => {
+                if (!disableStars) {
+                  setRating(index); /* only change star if not submitted */
+                }
+              }}
+              onMouseEnter={() => {
+                if (!disableStars) {
+                  setHover(index); /* only change star if not submitted */
+                }
+              }}
+              onMouseLeave={() => {
+                if (!disableStars) {
+                  setHover(rating); /* only change star if not submitted */
+                }
+              }}>
               <span className="star">&#9733;</span>
             </button>
           );
